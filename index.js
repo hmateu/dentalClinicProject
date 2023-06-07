80b271bd145be0dc22a31d8bb93326f2f7080f70
hmateu <hmateu.ortola@gmail.com>
2023-06-07 13:43:38 +0200

    feat: Creates the database and link it to the project

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const app = express();
-
 const PORT = 5000;
+const router = require('./router');
+const db = require('./db');
 
 app.use(express.json());
 
-app.listen(PORT, () => {
-    console.log("Servidor levantado y a la escucha en el puerto ",PORT);
-});
+app.use(router);
+
+db.then(
+    ()=>{
+        app.listen(PORT, () => {
+            console.log(`Servidor levantado correctamente y a la escucha en el puerto ${PORT}`);
+        });
+    }
+)
+.catch(
+    error => console.error(`Ha habido un error conectándose a la base de datos`, error)
+)
