bff5270a6c54e5f18fe25b8a15ac2327c1238035
hmateu <hmateu.ortola@gmail.com>
2023-06-08 22:50:08 +0200

    fix: Improves the query to display all appointments so that the corresponding name is displayed in each field instead of the foreign key

--- controllers/appointmentGetAllController.js
+++ controllers/appointmentGetAllController.js
@@ -1,9 +1,11 @@
 const { Appointment } = require("../models");
+const db = require("../models");
+const sequelize = require("sequelize");
 const appointmentGetAllController = {};
 
 appointmentGetAllController.getAllAppointments = async (req,res) => {
     try {
-        const appointments = await Appointment.findAll();
+        const appointments = await db.sequelize.query('select a.date as fecha, a.price as precio, a.assessment as conclusion, ud.name as dentista, up.name as paciente, s.name as servicio, p.name as forma_de_pago from Appointments a left join Users ud on a.dentist = ud.id left join Users up on a.patient = up.id left join Services s on a.service = s.id left join Payments p on a.payment = p.id', { type: sequelize.QueryTypes.SELECT });
         return res.json(
             {
                 success: true,
