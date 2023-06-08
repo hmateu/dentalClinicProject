c24ed09e4a80a2087eef980c8eb730e98fb3f123
hmateu <hmateu.ortola@gmail.com>
2023-06-08 09:41:27 +0200

    feat: Creates a post endpoint to login

---
+++ controllers/authLoginController.js
@@ -0,0 +1,65 @@
+const { User } = require('../models');
+
+const authLoginController = {};
+
+authLoginController.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+         //Validamos password
+         const userPassword = password;
+        
+         if (!password || !email) {
+             return res.json(
+                 {
+                     success: true,
+                     message: "Credenciales incorrectas"
+                 }
+             )
+         } 
+
+        const user = await User.findOne(
+            {
+                where: {
+                    email: email
+                }
+            }
+        );
+
+        if (!user) {
+            return res.json(
+                {
+                    success: true,
+                    message: "Usuario incorrecto"
+                }
+            )
+        }
+
+       
+
+        if(user.password == password){
+            return res.json(
+                {
+                    success: true,
+                    message: "Usuario logueado"
+                }
+            );
+        }
+        return res.json(
+            {
+                success: true,
+                message: "Contraseña incorrecta"
+            }
+        );
+
+    } catch (error) {
+        return res.status(500).json(
+            {
+                success: false,
+                message: "No puedes loguearte",
+                error: error
+            }
+        )
+    }
+}
+module.exports = authLoginController;
--- views/authRoutes.js
+++ views/authRoutes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const authRegisterController = require('../controllers/authRegisterController');
+const authLoginController = require('../controllers/authLoginController');
 
 router.post('/register', authRegisterController.register);
+router.post('/login', authLoginController.login);
 
 module.exports = router;
