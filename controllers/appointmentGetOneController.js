18ad965a2b05f93fb6442b5288572cba6fda4bf8
hmateu <hmateu.ortola@gmail.com>
2023-06-09 08:10:31 +0200

    feat: Creates a get endpoint to display an appointment in detail

---
+++ controllers/appointmentGetOneController.js
@@ -0,0 +1,28 @@
+const { Appointment } = require("../models");
+const db = require("../models");
+const sequelize = require("sequelize");
+const appointmentGetOneController = {};
+
+appointmentGetOneController.getOneAppointment = async (req,res) => {
+    try {
+        const appointmentId = req.params.id;
+        const appointment = await db.sequelize.query(`select a.date as fecha, a.price as precio, a.assessment as conclusion, ud.name as dentista, up.name as paciente, s.name as servicio, p.name as forma_de_pago from Appointments a left join Users ud on a.dentist = ud.id left join Users up on a.patient = up.id left join Services s on a.service = s.id left join Payments p on a.payment = p.id where a.id = ${appointmentId}`, { type: sequelize.QueryTypes.SELECT });
+        return res.json(
+            {
+                success: true,
+                message: "Cita recuperada",
+                data: appointment
+            }
+        )
+    } catch (error) {
+        return res.status(500).json(
+            {
+                success: false,
+                message: "No se puede mostrar la cita",
+                error: error.message
+            }
+        )
+    }
+}
+
+module.exports = appointmentGetOneController;
--- views/appointmentRoutes.js
+++ views/appointmentRoutes.js
@@ -1,6 +1,7 @@
 const appointmentCreateController = require('../controllers/appointmentCreateController');
 const appointmentDeleteController = require('../controllers/appointmentDeleteController');
 const appointmentGetAllController = require('../controllers/appointmentGetAllController');
+const appointmentGetOneController = require('../controllers/appointmentGetOneController');
 const appointmentGetOneDentistController = require('../controllers/appointmentGetOneDentistController');
 const appointmentGetOnePatientController = require('../controllers/appointmentGetOnePatientController');
 const appointmentUpdateController = require('../controllers/appointmentUpdateController');
@@ -8,6 +9,7 @@ const appointmentUpdateController = require('../controllers/appointmentUpdateCon
 const router = require('express').Router();
 
 router.get('/', appointmentGetAllController.getAllAppointments);
+router.get('/:id', appointmentGetOneController.getOneAppointment);
 router.get('/patient', appointmentGetOnePatientController.getOnePatientAppointments);
 router.get('/dentist', appointmentGetOneDentistController.getOneDentistAppointments);
 router.post('/', appointmentCreateController.createAppointment);
