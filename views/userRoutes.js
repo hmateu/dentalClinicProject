0602481cfafee292c13f030a7c12c1c3c1d4d6dc
hmateu <hmateu.ortola@gmail.com>
2023-06-09 14:45:17 +0200

    feat: Allows you to see registered users only if you are logged in

--- views/userRoutes.js
+++ views/userRoutes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
+const auth = require('../middlewares/verifyToken');
 const userDeleteController = require('../controllers/userDeleteController');
 const userGetAllController = require('../controllers/userGetAllController');
 const userGetOneController = require('../controllers/userGetOneController');
 const userUpdateController = require('../controllers/userUpdateController');
 
-router.get('/', userGetAllController.getAllUsers);
+router.get('/',auth, userGetAllController.getAllUsers);
 router.get('/:id', userGetOneController.getOneUser);
 router.put('/:id', userUpdateController.updateUser);
 router.delete('/:id', userDeleteController.deleteUser);
