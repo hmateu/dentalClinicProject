e68d0009316e344ce1e4348692f82f027202ee3e
hmateu <hmateu.ortola@gmail.com>
2023-06-12 22:16:43 +0200

    feat: Forces the password to have a good structure with regular expressions at the time of registration

--- controllers/authRegisterController.js
+++ controllers/authRegisterController.js
@@ -11,11 +11,22 @@ authRegisterController.register = async (req,res) => {
         const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
         const isValidEmail = emailRegex.test(email);
 
+        const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,15}$/;
+        const isValidPassword = passwordRegex.test(password);
+
         if(!isValidEmail){
             return res.json(
                 {
                     success: true,
                     message: "Email no válido"
+                }
+            )
+        }
+        if(!isValidPassword){
+            return res.json(
+                {
+                    success: true,
+                    message: "Contraseña no válida. Debe tener al menos una letra mayúscula. Debe tener al menos una letra minúscula. Debe tener al menos un número. Debe tener al menos un símbolo (puedes ajustar los símbolos permitidos según tus necesidades). Debe tener una longitud mínima de 8 caracteres. Debe tener una longitud máxima de 15 caracteres."
                 }
             )
         }
